03613e19ab5aaa0a89178ded2ecce94a18a98b66
Lovely <pub.nes@mail.ru>
2022-11-17 19:04:44 +0500

    make figure out total cost, button delete item from cart, btn to make order and clean cart

--- script.js
+++ script.js
@@ -6,11 +6,16 @@ const closeCart = document.querySelector('.cart-arrow')
 const url = 'https://dh.cubicle.53xapps.com/products';
 const productBox = document.querySelector('.product-box');
 const shopContent = document.querySelector('.shop-content');
-const  filterShow = document.querySelector('.filter-arrow');
+const filterShow = document.querySelector('.filter-arrow');
 const filterBox = document.querySelector('.filters-box');
 const filterClose = document.querySelector('.close-filter');
 const filter = document.querySelector('.filter-button');
-const cartItems = cart.querySelector('.cart-items').children;
+const cartItems = cart.querySelector('.cart-items');
+const shopContentItems = shopContent.children;
+const totalPrice = cart.querySelector('#total-cost');
+const deleteItemSvg = '<svg class="delete-btn-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56 64"><defs><style>.cls-1{fill:#ff2400;}.cls-2{fill:#ba1d08;}</style></defs><title>Delete from cart</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M42.48,64h-29a6,6,0,0,1-6-5.5L4,16H52L48.46,58.5A6,6,0,0,1,42.48,64Z"/><path class="cls-2" d="M52,8H38V6a6,6,0,0,0-6-6H24a6,6,0,0,0-6,6V8H4a4,4,0,0,0-4,4v4H56V12A4,4,0,0,0,52,8ZM22,6a2,2,0,0,1,2-2h8a2,2,0,0,1,2,2V8H22Z"/><path class="cls-2" d="M28,58a2,2,0,0,1-2-2V24a2,2,0,0,1,4,0V56A2,2,0,0,1,28,58Z"/><path class="cls-2" d="M38,58h-.13A2,2,0,0,1,36,55.88l2-32a2,2,0,1,1,4,.25l-2,32A2,2,0,0,1,38,58Z"/><path class="cls-2" d="M18,58a2,2,0,0,1-2-1.87l-2-32a2,2,0,0,1,4-.25l2,32A2,2,0,0,1,18.13,58Z"/></g></g></svg>';
+const makeOrder = cart.querySelector('.make-order');
+
 
 //Минус у счетчика в карточке на странице
 function makeMinusCounter(){
@@ -25,8 +30,43 @@ function makeMinusCounter(){
         }else{
             number.textContent = 1;
         }
-
 }
+//Счетчик минуса для корзины
+function makeMinusForCartCounter(){
+    let number = this.nextElementSibling;
+    let productBox = this.closest('.product-box');
+    let price = productBox.querySelector('.price');
+    const id = productBox.id;
+
+    const nominalBlock = [...shopContentItems].find(elem => elem.id == id);
+    const nominalPrice = nominalBlock.querySelector('.price');
+
+           
+        if(number.textContent > 1){
+            number.textContent--;
+            price.textContent -=  nominalPrice.textContent;
+            totalPrice.textContent -= nominalPrice.textContent;
+            
+        }else{
+            number.textContent = 1;
+        }
+}
+//Счетчик плюса для корзины
+function makePlusForCartCounter(){ 
+    let productBox = this.closest('.product-box');
+    let price = productBox.querySelector('.price'); 
+    const id = productBox.id;
+
+    const nominalBlock = [...shopContentItems].find(elem => elem.id == id);
+    const nominalPrice = nominalBlock.querySelector('.price');
+    
+    this.previousElementSibling.textContent++;
+    price.textContent = Number(price.textContent) + Number(nominalPrice.textContent);
+    totalPrice.textContent = Number(totalPrice.textContent) + Number(nominalPrice.textContent);
+
+    
+}
+
 //Плюс у счетчика в карточке на странице
 function makePlusCounter(){ 
     let productBox = this.closest('.product-box');
@@ -40,24 +80,45 @@ function makePlusCounter(){
 
 //добавить товар в корзину
 function addToCart(){
-    let cartItem = this.closest('.product-box').cloneNode(true);
-    let cartBox = cart.querySelector('.cart-items');
-    let price = cartItem.querySelector('.price');
-    let totalCost = document.getElementById('total-cost');
-    let counterNumber = cartItem.querySelector('.counter-number');
-    counterNumber.previousElementSibling.addEventListener('click',makeMinusCounter);
-    counterNumber.nextElementSibling.addEventListener('click',makePlusCounter);
+    const cartItem = this.closest('.product-box').cloneNode(true);
+    const cartBox = cart.querySelector('.cart-items');
+    const price = cartItem.querySelector('.price');
+    const totalCost = document.getElementById('total-cost');
+    const counterNumber = cartItem.querySelector('.counter-number');
+
+    counterNumber.previousElementSibling.addEventListener('click',makeMinusForCartCounter);
+    counterNumber.nextElementSibling.addEventListener('click',makePlusForCartCounter);
     //Удаление кнопки добавить в корзину у клона
     cartItem.removeChild(cartItem.querySelector('button'));
+    
+    const btn = document.createElement('span');
+    btn.className = 'delete-btn';
+    btn.innerHTML = deleteItemSvg;
+
+    btn.addEventListener('click', deleteItemFromCart);
+
+    cartItem.appendChild(btn);
     //Проверка есть ли предмет в корзине или нет
-    findItemInCart(cartItems,totalCost,price,counterNumber,cartBox,cartItem);
+    findItemInCart(cartItems.children,totalCost,price,counterNumber,cartBox,cartItem);
     
 }
+
+
+
+//Удаление товара из корзины
+function deleteItemFromCart(){
+    const parent = this.closest('.product-box');
+    const price = parent.querySelector('.price');
+    
+    totalPrice.textContent = Number(totalPrice.textContent) - Number(price.textContent);
+    cartItems.removeChild(parent);
+}
+
 
 function findItemInCart(arr,totalCost,price,counterNumber,cartBox,cartItem){
     let itemsArr = [...arr];
     let good = itemsArr.find(elem=> elem.id == cartItem.id);
-
+    
     if (good){
         totalCost.textContent = Number(totalCost.textContent) + Number(price.textContent) * counterNumber.textContent;
         good.querySelector('.counter-number').textContent = Number(good.querySelector('.counter-number').textContent) + Number(counterNumber.textContent);
@@ -104,24 +165,32 @@ const renderProducts = function(list){
        let  {id,category_id:categoryId,title,description,photo,price,brand_id:brandId} = list[i];
 
         const newBox = new Product(id,categoryId,title,description,photo,price,brandId);
-        
-        let box = productBox.cloneNode(true);
-        box.price = newBox.price;
+        newBox.title = newBox.title.replace('Смартфон', '');
+        newBox.title = newBox.title.replace('Apple', '');
+        //Копирование структуры карточки и ее генерация в контейнере
+        copyBlock(newBox);
 
-        box.querySelector('.product-img').src = newBox.photo.front;
-        box.querySelector('.product-title').textContent = newBox.title ;
-        box.querySelector('.price').textContent = newBox.price;
-
-        box.querySelector('.counter_minus').addEventListener('click',makeMinusCounter);
-        box.querySelector('.counter_plus').addEventListener('click',makePlusCounter);
-        box.querySelector('.add-to-cart').addEventListener('click',addToCart);
-
-        box.id = newBox.id;
-        box.dataset.category = newBox.category;
-        box.dataset.brandId = newBox.brandId;
-        shopContent.appendChild(box);
     }
 }
+
+function copyBlock(newBox){
+    let box = productBox.cloneNode(true);
+    box.price = newBox.price;
+
+    box.querySelector('.product-img').src = newBox.photo.front;
+    box.querySelector('.product-title').textContent = newBox.title ;
+    box.querySelector('.price').textContent = newBox.price;
+
+    box.querySelector('.counter_minus').addEventListener('click',makeMinusCounter);
+    box.querySelector('.counter_plus').addEventListener('click',makePlusCounter);
+    box.querySelector('.add-to-cart').addEventListener('click',addToCart);
+
+    box.id = newBox.id;
+    box.dataset.category = newBox.category;
+    box.dataset.brandId = newBox.brandId;
+    shopContent.appendChild(box);
+}
+
 
 //Открытие фильтров
 filterShow.addEventListener('click',()=>{
@@ -159,4 +228,14 @@ function hideElements(arr){
         element.classList.add('hide');
     });
 }
-
+//Делаем заказ и очищаем корзину
+makeOrder.addEventListener('click',function(){
+    if (cartItems.children.length > 0) {
+        alert("We are making you order, thank you for choosing our store");
+        [...cartItems.children].forEach(elem => cartItems.removeChild(elem));
+        totalPrice.textContent = 0;
+    } else{
+        alert('Sorry, but you didn\'t take anything')
+    }
+    
+})
